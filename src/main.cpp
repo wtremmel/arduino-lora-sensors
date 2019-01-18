af5da14579410df652ad90dfe2126ea62f7860c9
Wolfgang Tremmel <wtremmel@garf.de>
2019-01-18 14:01:23 +0100

    dynamic sleep time

--- src/main.cpp
+++ src/main.cpp
@@ -251,9 +251,9 @@ void read_voltage() {
   float v = my_voltage();
 
   Log.verbose(F("Voltage is %D"), v);
-  
+
   if (v > 0.1) {
-    sleeptime = (int)(10000.0 / (v ^ 4.0));
+    sleeptime = (int)(10000.0 / pow(v,4.0));
   } else {
     sleeptime = 90;
   }
